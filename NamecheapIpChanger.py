67b00e892524d131f6727f32fa6fc7090e2bb41d
Vitalii Rybalko <vilko.a@gmail.com>
2020-07-10 04:39:22 +0300

    debug logging to a file in NamecheapIpChanger
    Signed-off-by: Vitalii Rybalko <vilko.a@gmail.com>

--- NamecheapIpChanger.py
+++ NamecheapIpChanger.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
+import os
+import logging
 from selenium import webdriver
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 from random import randint
 from dotenv import load_dotenv
-import os
 from xvfbwrapper import Xvfb
 
 from ExternalIp import ExternalIp
 
 
-
 class NamecheapIpChanger:
 
     def __init__(self, external_ip=None):
+        logging.basicConfig(filename='namecheap_ip_changer.log', level=logging.DEBUG)
         self.vdisplay = Xvfb(width=1920, height=1080, colordepth=24, fbdir="/tmp/")
         self.vdisplay.start()
 
@@ -42,70 +43,70 @@ class NamecheapIpChanger:
 
 
     def _login(self):
-        print('logging in namecheap')
+        logging.debug('logging in namecheap')
         self.driver.get("https://www.namecheap.com/myaccount/login/")
 
         self._random_delay(10)
 
         self._enter_input("input[name=LoginUserName].nc_username",os.getenv('NAMECHEAP_USERNAME'))
 
-        print('username entered')
+        logging.debug('username entered')
         self._random_delay(10)
 
         self._enter_input("input[name=LoginPassword].nc_password",os.getenv('NAMECHEAP_PASSWORD'))
 
-        print('password entered')
+        logging.debug('password entered')
         self._random_delay(5)
 
         self._css_selector_click('input[type=submit].nc_login_submit')
-        print('submit pressed')
+        logging.debug('submit pressed')
 
     def __find_host_records_table(self):
         self._random_delay(5)
-        print('change url to domain control panel')
+        logging.debug('change url to domain control panel')
         self.driver.get('https://ap.www.namecheap.com/Domains/DomainControlPanel/{}/advancedns'.format(os.getenv('NAMECHEAP_DOMAIN_NAME')))
         sleep(3)
         self._random_delay(2)
 
         host_records_table = self.driver.find_elements_by_css_selector('table')[0]
-        print('host_records_table', host_records_table)
+        logging.debug('host_records_table {}'.format(host_records_table))
 
-        print('scrolling to table with host records')
+        logging.debug('scrolling to table with host records')
         self.driver.execute_script("arguments[0].scrollIntoView(true);", host_records_table);
         self.driver.execute_script("arguments[0].scrollIntoView(true);", host_records_table);
         return host_records_table
 
     def __enter_external_ip_to_records_table(self, host_records_table):
         ip_inputs = host_records_table.find_elements_by_css_selector('td.value')
-        print('ip_inputs', ip_inputs)
+        logging.debug('ip_inputs {}'.format(ip_inputs))
 
         for p in ip_inputs:
-            print('p.click()')
+            logging.debug('p.click()')
             try:
                 p.click()
             except ElementClickInterceptedException as e:
-                print('ElementClickInterceptedException')
+                logging.debug('ElementClickInterceptedException')
                 pass
             self._random_delay(2)
 
-        print('clicked on the all ip inputs')
+        logging.debug('clicked on the all ip inputs')
 
         inputs = self.driver.find_elements_by_css_selector('td.value input')
         for i in inputs:
             self._random_delay(2)
             self.driver.execute_script('arguments[0].value = "{}"'.format(self.external_ip), i)
 
-        print('changed ip to {}'.format(self.external_ip))
+        logging.debug('changed ip to {}'.format(self.external_ip))
 
         save_button = self.driver.find_element_by_css_selector('table.advanced-dns a.text-green')
         save_button.click()
 
-        print('pressed save button')
+        logging.debug('pressed save button')
 
 
     def _random_delay(self, max_value=10):
         delay = randint(0, max_value) 
-        print('self._random_delay max: {}, selected: {}'.format(max_value, delay))
+        logging.debug('self._random_delay max: {}, selected: {}'.format(max_value, delay))
         sleep(delay)
 
     def _css_selector_click(self, css_selector):
